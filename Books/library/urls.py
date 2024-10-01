d0b8a4da43e82980d310de10c62ddb481a18446b
Winnie Jomo <winniejomo89@gmail.com>
2024-10-01 23:28:40 +0300

    added update and create

--- Books/library/urls.py
+++ Books/library/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import create_new_book,update_existing_book
 
 urlpatterns = [
     path('', views.home,name='home'),
     path('books/', views.book_list, name='book_list'),
-    path('books/<int:pk>/', views.book_detail, name='book_detail')
-
+    path('books/<int:pk>/', views.book_detail, name='book_detail'),
+    path('create_new_book/', create_new_book, name='create_new_book'),
+    path('update_existing_book/<int:pk>/', update_existing_book, name='update_existing_book')
 ]
--- Books/library/views.py
+++ Books/library/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import Book
+from django.http import JsonResponse
+from .forms import BookForm
 
 
 def home(request):
@@ -14,3 +16,30 @@ def book_list(request):
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'book_detail.html', {'book':book})
+
+# creating a new book
+def create_new_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list') 
+    else:
+        form = BookForm() 
+
+   
+    return render(request, 'create_new_book.html', {'form': form})
+
+# Update an existing book
+def update_existing_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list') 
+    else:  
+        form = BookForm(instance=book)  
+
+    return render(request, 'update_existing_book.html', {'form': form})
